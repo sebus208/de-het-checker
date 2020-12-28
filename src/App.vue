0fac68bfca28932f16056985a7555fdaf020124a
sebus208 <kovewebdesign@gmail.com>
2020-12-29 00:01:19 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app>
+
+    <!-- TopBar -->
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="20"
+        />
+
+        <h1>DeHet</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/djmbritt/dehet"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Github</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+
+    <!-- Body -->
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+
+    <!-- Footer -->
+    <v-footer absolute>
+      <v-col
+        class="text-center"
+        cols="12"
+        max-height="10px"
+        inset=true
+        padless=true
+      >
+        Made with ❤ in Amsterdam.
+      </v-col>
+    </v-footer>
+
+
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
