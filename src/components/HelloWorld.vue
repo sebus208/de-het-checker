64201228305a1b417c318cd656beffc1ce292115
D.J.M. Britt <david.britt@hva.nl>
2020-12-30 21:58:07 +0100

    Added output component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,25 +2,23 @@
   <v-container>
     <v-row class="text-center">
       <v-col class="mb-4">
+        
         <v-skeleton-loader v-if="loading" :loading="loading" v-bind="attrs" transition="scale-transition"></v-skeleton-loader>
 
         <v-textarea v-show="loaded" v-bind="attrtxt" transition="scale-transition" v-model="attrtxt.textValue"></v-textarea>
 
-        <!-- TODO this should be it's own component. -->
-        <!-- For now let's just fill this in with the following text. We still need to work the text and output it nicely. -->
-        <p>{{ attrtxt.textValue == '' ? '' :  winkTokenize(attrtxt.textValue) }}</p>
-
+        <Output :textData="attrtxt.textValue"></Output>
+        
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-// Imports
-const WinkTokenizer = require('wink-tokenizer');
-const wink = new WinkTokenizer(); // we'll be using the tokenize method on this one later.
+import Output from './Output.vue';
 
 export default {
+  components: { Output },
   name: 'HelloWorld',
 
   data: () => ({
@@ -32,7 +30,7 @@ export default {
     // TextContent
     textContent: '',
 
-    // styling, TODO should this be here?
+    // styling for the skeletonLoader
     attrs: {
       elevation: 2,
       class: 'mx-auto',
@@ -42,7 +40,7 @@ export default {
       // transition: "scale-transition"
     },
 
-    // Styling for the text, should this be here?
+    // Styling for the textbox. 
     attrtxt: {
       textValue: "Dit boek is het enige dat de brand heeft overleefd.",
       class: 'mx-auto',
@@ -57,20 +55,6 @@ export default {
       noResize: true,
     },
   }), // data
-
-  methods: {
-    /**
-     * this method returns an array of each word or punctuation mark of the inputted sentence.
-     * 
-     * Input: Hoi! hoe gaat het?
-     * Output: [ { "value": "Hoi", "tag": "word" }, { "value": "!", "tag": "punctuation" }, 
-     *            { "value": "hoe", "tag": "word" }, { "value": "gaat", "tag": "word" }, 
-     *          { "value": "het", "tag": "word" }, { "value": "?", "tag": "punctuation" } ]
-     */
-    winkTokenize: function (value) {
-      return wink.tokenize(value);
-    },
-  }, // methods
 
   // This is for the skeleton loader, but it's stupid.
   // It doesn't properly wait for the data to load, it just waits a second before loading the actual text box.
---
+++ src/components/Output.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>{{ textData == '' ? '' : tokenize(textData) }}</p>
+  </div>
+</template>
+
+<script>
+const WinkTokenizer = require('wink-tokenizer');
+const wink = new WinkTokenizer(); // we'll be using the tokenize method on this one later.
+
+export default {
+  name: 'Output',
+  props: ['textData'],
+  methods: {
+    /**
+     * this method returns an array of each word or punctuation mark of the inputted sentence.
+     *
+     * Input: Hoi! hoe gaat het?
+     * Output: [ { "value": "Hoi", "tag": "word" }, { "value": "!", "tag": "punctuation" },
+     *            { "value": "hoe", "tag": "word" }, { "value": "gaat", "tag": "word" },
+     *          { "value": "het", "tag": "word" }, { "value": "?", "tag": "punctuation" } ]
+     */
+    tokenize: function (value) {
+      return wink.tokenize(value);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
