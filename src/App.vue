8c9477034ba3384d3bd191ffef12da55494d1b3c
sebus208 <kovewebdesign@gmail.com>
2020-12-30 22:29:08 +0100

    Disabled vue/experimental-script-setup-vars

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
   <v-app>
-
     <!-- TopBar -->
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -22,36 +17,21 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/djmbritt/dehet"
-        target="_blank"
-        text
-      >
+      <v-btn href="https://github.com/djmbritt/dehet" target="_blank" text>
         <span class="mr-2">Github</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
-
     <!-- Body -->
     <v-main>
-      <HelloWorld/>
+      <HelloWorld />
     </v-main>
 
     <!-- Footer -->
     <v-footer absolute>
-      <v-col
-        class="text-center"
-        cols="12"
-        max-height="10px"
-        inset=true
-        padless=true
-      >
-        Made with ❤ in Amsterdam.
-      </v-col>
+      <v-col class="text-center" cols="12" max-height="10px" inset="true" padless="true"> Made with ❤ in Amsterdam. </v-col>
     </v-footer>
-
-
   </v-app>
 </template>
 
